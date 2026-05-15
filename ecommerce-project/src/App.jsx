285303c2a181353a38ba2c1d320d31cc76e3a16b
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-05-15 19:11:50 +0530

    Added routing and separated files

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,11 +1,18 @@
+import {Routes, Route} from 'react-router';
 import { HomePage } from './pages/Homepage';
+import {CheckoutPage} from './pages/Checkoutpage';
+import { OrdersPage } from './pages/Orderspage';
 import './App.css'
 
 function App() {
  
 
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+    </Routes>
   )
 }
 
