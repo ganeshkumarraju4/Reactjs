e0a1b5d8f5c5043f040d823d5dcca7740ded0236
ganeshkumarraju4 <ganeshkumarraju4@gmail.com>
2026-05-23 16:28:44 +0530

    Create delete feature in cart

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -4,22 +4,22 @@ import { CartItemDetails } from './CartItemDetails';
 export function OrderSummary({ cart, deliveryOptions,loadCart }){
     return(
           <div className="order-summary">
-                       {deliveryOptions.length > 0 &&cart.map((cartItem)=>{
-                        
-                        return(
-                             <div key ={cartItem.productId}className="cart-item-container">
-                            <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions}/>
-        
-                            <div className="cart-item-details-grid">
-                           <CartItemDetails cartItem = {cartItem}/>
-        
-                          <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
-                            </div>
-                        </div>
-                        );
-                       })}
-        
-                      
-                        </div>
+            {deliveryOptions.length > 0 &&cart.map((cartItem)=>{
+            
+            return(
+            <div key ={cartItem.productId}className="cart-item-container">
+                <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions}/>
+
+                <div className="cart-item-details-grid">
+                <CartItemDetails cartItem = {cartItem} loadCart={loadCart}/>
+
+                <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart} />
+            </div>
+            </div>
+            );
+            })}
+
+            
+            </div>
     );
 }
